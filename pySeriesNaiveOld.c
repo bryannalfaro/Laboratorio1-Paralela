96c656a2ad5465300a6141aaf262a8eeb92b4693
Bryann Alfaro <alf19372@uvg.edu.gt>
2023-02-15 20:41:53 -0600

    ADD: Exercises a to d

--- pySeriesNaiveOld.c
+++ pySeriesNaiveOld.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 
 int main(int argc, char* argv[]) {
     double factor = 1.0;
     double sum = 0.0;
-    int k, n;
+    int k, n = 1500;
+    int thread_count = 4;
     double pi_approx;
-
+    #pragma omp parallel for num_threads(thread_count) reduction(+:sum)
     for (k = 0; k < n; k++) {
-    sum  += factor/(2*k+1);
-    factor = -(factor);
+      sum  += factor/(2*k+1);
+      factor = -(factor);
     }
     pi_approx = 4.0*sum;
 
---
+++ pySeriesSeq.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+    double factor = 1.0;
+    double sum = 0.0;
+    int k, n = 1500;
+    double pi_approx;
+
+    for (k = 0; k < n; k++) {
+    sum  += factor/(2*k+1);
+    factor = -(factor);
+    }
+    pi_approx = 4.0*sum;
+
+     printf("pi_approx = %f", pi_approx);
+
+  return 0;
+}
