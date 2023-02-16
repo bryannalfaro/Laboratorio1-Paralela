988eabb21895415a74c713548427dc4d61723f48
Méndez <47764656+El-Mendez@users.noreply.github.com>
2023-02-15 20:50:38 -0600

    finished program

--- pySeriesNaiveD.c
+++ pySeriesNaiveD.c
@@ -7,15 +7,15 @@ int main(int argc, char* argv[]) {
     double factor = 1.0;
     double sum = 0.0;
     int k, n = 10e6;
-    int thread_count = 4;
+    int thread_count = 1;
     double pi_approx;
     #pragma omp parallel for num_threads(thread_count) reduction(+:sum)
     for (k = 0; k < n; k++) {
       if (k % 2 == 0)
-            factor = 1.0;
-          else
-            factor = -1.0;
-          sum  += factor/(2*k+1);
+        factor = 1.0;
+      else
+        factor = -1.0;
+      sum  += factor/(2*k+1);
     }
     pi_approx = 4.0*sum;
 
---
+++ pySeriesNaiveF.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+int main(int argc, char* argv[]) {
+    double factor = 1.0;
+    double sum = 0.0;
+    int k, n = 10e6;
+    int thread_count = 4;
+    double pi_approx;
+    #pragma omp parallel for num_threads(thread_count) \
+            reduction(+:sum) private(factor)
+    
+    for (k = 0; k < n; k++) {
+      if (k % 2 == 0)
+        factor = 1.0;
+      else
+        factor = -1.0;
+      sum  += factor/(2*k+1);
+    }
+    pi_approx = 4.0*sum;
+
+    printf("pi_approx = %f", pi_approx);
+
+  return 0;
+}
