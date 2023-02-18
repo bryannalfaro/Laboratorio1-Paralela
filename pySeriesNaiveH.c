8563df4f0b1ad098cc3d46140fd2b0830b86237a
oscar-paredez <oaparedez17@gmail.com>
2023-02-17 21:43:23 -0600

    2H

--- pySeriesNaiveH.c
+++ pySeriesNaiveH.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define BLOCK_SIZE 16
+#define BLOCK_SIZE 128
 
 int main(int argc, char* argv[]) {
     double factor = 1.0;
     double sum = 0.0;
-    int k, n = 10e6;
-    int thread_count = 4;
+    int k, n = 10e7;
+    int thread_count = 10;
     double pi_approx;
+    double seq_time = 0.3681;
+
+    double start, end,delta;
+    start = omp_get_wtime();
     #pragma omp parallel for num_threads(thread_count) \
-            reduction(+:sum) private(factor) schedule(static, BLOCK_SIZE)
+            reduction(+:sum) private(factor) schedule(auto)
     
     for (k = 0; k < n; k++) {
       if (k % 2 == 0)
@@ -22,7 +26,19 @@ int main(int argc, char* argv[]) {
     }
     pi_approx = 4.0*sum;
 
+    end = omp_get_wtime();
+    delta = end - start;
+    printf("threads = %d", thread_count);
+    printf("\n");
+    printf("n = %d", n);
+    printf("\n");
     printf("pi_approx = %f", pi_approx);
+    printf("\n");
+    printf("Time: %f seconds", delta);
+    printf("\n");
+    printf("Speedup: %f", seq_time/delta);
+    printf("\n");
+    printf("Efficiency: %f", (seq_time/delta)/thread_count);
 
   return 0;
 }
